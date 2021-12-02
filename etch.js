01f57c3fba36d6563c6934cbefb724f9b80c127a
Austin Keller <austinkllr@gmail.com>
2021-12-01 20:28:57 -0800

    added hover effect to change color of squares

--- etch.js
+++ etch.js
@@ -5,3 +5,13 @@ for (let i = 0; i < 256; i++) {
     squareDiv.classList.add('square');
     gridContainer.appendChild(squareDiv);
 }
+
+const allSquareDivs = document.querySelectorAll('.square');
+
+allSquareDivs.forEach(square => (square.addEventListener('mouseenter', changeColor)));
+
+function changeColor(square) {
+    square.target.style.backgroundColor = '#000000';
+}
+
+const clearBtn = document.querySelector('button');
